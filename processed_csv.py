513d089826bac922d78f41192b2edfcee7b698f4
Aleksandra <112889744+alchrt@users.noreply.github.com>
2024-08-25 14:09:16 +0000

    Updated processed_csv.py

--- processed_csv.py
+++ processed_csv.py
@@ -1,8 +1,12 @@
-import pandas as pd  # type: ignore
+import pandas as pd
+import numpy as np
 
 # reading the source file
-url = 'https://.../online-file.csv',  # online EURPLN rate file that will be processed
-data = pd.read_csv(url, encoding='latin1', sep=';')
+data = pd.read_csv(
+    'https://static.nbp.pl/dane/kursy/Archiwum/archiwum_tab_a_2024.csv',  # online EURPLN rate file that will be processed
+    encoding='latin1',
+    sep=';'
+)
 
 # keeping selected columns only; df.copy() not needed here as filtering already creates a new df
 df = data[['data', '1EUR']]
@@ -10,19 +14,21 @@ df = data[['data', '1EUR']]
 # renaming the columns
 df = df.rename(
     columns={
-        'date': 'Date',
-        '1EUR': 'EUR'
-    },
-    inplace=True
+        df.columns[0]: 'Date',
+        df.columns[1]: 'EUR'
+    }
 )
 
 # filtering the Date column to only contain rows with dates
-df = df[df['Date'].str.startswith('202', na=False)].reset_index(drop=True)
+df = df[df['Date'].str.startswith('202', na=False)]
+
+# adding an index value starting from 1 (for index starting from 0: adding .reset_index(drop=True) to the previous step would suffice)
+df.index = np.arange(1, len(df)+1)
 
 # converting values
 df['EUR'] = df['EUR'].str.replace(',', '.')
 df['EUR'] = pd.to_numeric(df['EUR'], errors='coerce')    # converting exchange rates to numbers
-df['Date'] = pd.to_datetime(df['Date']).dt.normalize()  # converting to date but keeping the datetime type by normalizing
+df['Date'] = pd.to_datetime(df['Date']).dt.date     # converting to date
 
 print(df.dtypes)  # to double-check the data types
 print(f'\nDate range: {df.Date.min()}/{ df.Date.max()}')    # to view the current date range
